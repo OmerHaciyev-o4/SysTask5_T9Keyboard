89408a820f93dfa98b25576e092062df3b2d04d2
OmerHaciyev-o4 <omer04118102@gmail.com>
2021-12-14 00:23:58 +0400

    Sys5Task_T9Keyboard

---
+++ Sys5Task_T9Keyboard/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sys5Task_T9Keyboard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Sys5Task_T9Keyboard/MVVM/Views/T9Keyboard.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Sys5Task_T9Keyboard.MVVM.ViewModels;
+
+namespace Sys5Task_T9Keyboard.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for T9Keyboard.xaml
+    /// </summary>
+    public partial class T9Keyboard : Window
+    {
+        public T9Keyboard()
+        {
+            InitializeComponent();
+
+            var vm = new ViewModel() {T9Keyboard = this};
+            this.DataContext = vm;
+        }
+    }
+}
